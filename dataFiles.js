c98c855c05765673dc341c51f5267f85c6bd4deb
sm-sadi <63817831+sm-sadi@users.noreply.github.com>
2021-01-30 14:39:01 +0100

    initial files

---
+++ dataFiles.js
@@ -0,0 +1,70 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD6cbCg2TUBHQajwuNxkweRrbd1dty4BsM",
+    authDomain: "contact-form-1c50b.firebaseapp.com",
+    databaseURL: "https://contact-form-1c50b-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "contact-form-1c50b",
+    storageBucket: "contact-form-1c50b.appspot.com",
+    messagingSenderId: "130692679589",
+    appId: "1:130692679589:web:55c55bd7498eb5a078ca4c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   firebase.initializeApp(Config);
+
+//   refernce messages collection 
+ var messageRef = firebase.database().ref('/messages');
+  
+//   event listeener from the from 
+const contactForm = document.getElementById("contact-form");
+const userName = document.getElementById("userName");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+contactForm.addEventListener("submit", handelsubmit);
+
+// handlle submit 
+function handelsubmit(Event){    
+    Event.preventDefault();
+let userNameValue = userName.value;
+let emailValue = email.value
+let messageValue = message.value
+
+
+    console.log(` username : ${userNameValue} ..
+    username : ${emailValue} ..
+    username : ${messageValue} ..`);
+    // save message to database 
+    saveMessage(userNameValue, emailValue ,messageValue);
+    // clearing form after input is done 
+    // clearForm()
+}
+
+// clear from after input 
+
+function clearForm(){
+    userName.value ='';
+    email.value ='';
+    message.value='';
+}
+
+// save the message 
+function saveMessage( userNameValue , emailValue , messageValue){
+//    var newMessageRef = messageRef.push();
+    // newMessageRef.set({
+    //     name : userNameValue,
+    //     email :  emailValue,
+    //     message : messageValue
+    // }
+    // );
+    var newMessageRef = {
+            name : userNameValue,
+            email :  emailValue,
+            message : messageValue
+        }
+        messageRef.push(newMessageRef);
+       
+
+}
